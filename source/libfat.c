1288d8f8ec9008317a89d8927720c1389a73db17
Michael Chisholm <chishm@users.sourceforge.net>
2006-08-14 11:59:25 +0000

    Added extended devoptab_t functions

--- source/libfat.c
+++ source/libfat.c
@@ -55,7 +55,14 @@ const devoptab_t dotab_fat = {
 	_FAT_stat_r,
 	_FAT_link_r,
 	_FAT_unlink_r,
-	_FAT_chdir_r
+	_FAT_chdir_r,
+	_FAT_rename_r,
+	_FAT_mkdir_r,
+	sizeof (DIR_STATE_STRUCT),
+	_FAT_diropen_r,
+	_FAT_dirreset_r,
+	_FAT_dirnext_r,
+	_FAT_dirclose_r
 };
 
 bool fatInit (u32 cacheSize, bool setAsDefaultDevice) {
